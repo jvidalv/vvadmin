cd6fb1e4d79511b3806056bb3b6976e2418bd879
Josep Vidal <josepvidalvidal@gmail.com>
2020-06-04 20:56:39 +0200

    💧 Improved the user token api from Astrale

--- src/models/astrale/User.php
+++ src/models/astrale/User.php
@@ -40,6 +40,6 @@ class User extends \yii\mongodb\ActiveRecord
      */
     public function attributes()
     {
-        return ['_id', 'expo_token', 'language',  'name', 'birthDate', 'sign', 'relationship', 'sex', 'number', 'created_at'];
+        return ['_id', 'expo_token', 'language',  'name', 'birthDate', 'sign', 'relationship', 'sex', 'number', 'error', 'created_at'];
     }
 }
